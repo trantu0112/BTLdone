03d6e42cd9d12a2fcaf6169942707624149fb117
Tran Tu <trantu01121999@gmail.com>
2021-01-07 14:17:19 +0700

    note

--- Picking.js
+++ Picking.js
@@ -6,24 +6,18 @@ var VSHADER_SOURCE =
   'attribute float a_Face;\n' +   // Số bề mặt (Không thể sử dụng int cho biến thuộc tính)
   'attribute vec4 a_Normal;\n' +        // Normal
   'uniform mat4 u_MvpMatrix;\n' +
-  /////
+  ///// start thêm 1 //////////
   'uniform mat4 u_ModelMatrix;\n' +    // Model matrix
   'uniform mat4 u_NormalMatrix;\n' +   // Coordinate transformation matrix of the normal
   'uniform vec3 u_LightColor;\n' +     // Light color
   'uniform vec3 u_LightPosition;\n' +  // Position of the light source
   'uniform vec3 u_AmbientLight;\n' +   // Ambient light color
-  /////
+  ///// end thêm 1 //////////
   'uniform int u_PickedFace;\n' + // Số bề mặt của khuôn mặt đã chọn (biến đồng nhất)
   'varying vec4 v_Color;\n' +   // Biến thay đổi
   'void main() {\n' +
   '  gl_Position = u_MvpMatrix * a_Position;\n' + // Phép biến đổi hình chiếu chế độ xem mô hình trên vị trí đỉnh
-  // Make the length of the normal 1.0
-  // '  vec3 normal = normalize(a_Normal.xyz);\n' +
-  // // Dot product of the light direction and the orientation of a surface (the normal)
-  // '  float nDotL = max(dot(u_LightDirection, normal), 0.0);\n' +
-  // // Calculate the color due to diffuse reflection
-  // '  vec3 diffuse = u_LightColor * a_Color.rgb * nDotL;\n' +
-  ////
+  ///// start thêm 2 //////////
        // Recalculate the normal based on the model matrix and make its length 1.
   '  vec3 normal = normalize(vec3(u_NormalMatrix * a_Normal));\n' +
      // Calculate world coordinate of vertex
@@ -36,12 +30,13 @@ var VSHADER_SOURCE =
   '  vec3 diffuse = u_LightColor * a_Color.rgb * nDotL;\n' +
      // Calculate the color due to ambient reflection
   '  vec3 ambient = u_AmbientLight * a_Color.rgb;\n' +
-  /////
-  '  int face = int(a_Face);\n' + // Chuyển đổi thành int
-  '  vec3 color = (face == u_PickedFace) ? vec3(1.0, 1.0, 1.0) : diffuse + ambient;\n' + //Tất cả các thành phần đều = Nếu là 1 , nó là màu đen tại thời điểm này; nếu không, màu của đỉnh vẫn là màu trước đó
-  '  if(u_PickedFace == 0) {\n' + // nếu click = 0, hãy đặt số mặt thành v_Color
-  // Ba thành phần đầu tiên RGB và thành phần thứ tư được sử dụng để xác định đỉnh nào được nhấp Mặt
-  '    v_Color = vec4(color, a_Face/255.0);\n' + // Thành phần thứ tư của màu làm cho độ trong suốt của đồ họa khác đi
+  ///// end thêm 2 //////////
+  '  int face = int(a_Face);\n' + 
+  ///// start sửa 1 (a_Color.rgb -> diffuse + ambient) //////////
+  '  vec3 color = (face == u_PickedFace) ? vec3(1.0, 1.0, 1.0) : diffuse + ambient;\n' +
+  ///// end sửa 1 //////////
+  '  if(u_PickedFace == 0) {\n' + 
+  '    v_Color = vec4(color, a_Face/255.0);\n' + 
   '  } else {\n' +
   '    v_Color = vec4(color, a_Color.a);\n' +
   '  }\n' +
@@ -60,7 +55,9 @@ var FSHADER_SOURCE =
 var ANGLE_STEP = 0.0; // Góc xoay (độ / giây)
 var g_currentAngle = 0.0; // Góc quay hiện tại
 //var g_angleStepUD = 0.0;
+///// start thêm 3 //////////
 var g_angleStepUD = 0.0;
+///// end thêm 3 //////////
 
 function main() {
   // Gọi phần tử canvas
@@ -92,17 +89,17 @@ function main() {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.enable(gl.DEPTH_TEST);
 
-  // Lấy mô hình vị trí lưu trữ của ma trận hình chiếu khung nhìn, lấy vị trí của bề mặt nhấp chuột
-  //var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+  
+  
   var u_PickedFace = gl.getUniformLocation(gl.program, 'u_PickedFace');
-  //var u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor');//add phép chiếu
-  //var u_LightDirection = gl.getUniformLocation(gl.program, 'u_LightDirection');//add phép chiếu
-  var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
   var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+  ///// start thêm 4 //////////
+  var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
   var u_NormalMatrix = gl.getUniformLocation(gl.program, 'u_NormalMatrix');
   var u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor');
   var u_LightPosition = gl.getUniformLocation(gl.program, 'u_LightPosition');
   var u_AmbientLight = gl.getUniformLocation(gl.program, 'u_AmbientLight');
+  ///// end thêm 4 //////////
 
   if (!u_MvpMatrix || !u_PickedFace) { 
     console.log('Failed to get the storage location of uniform variable');
@@ -111,22 +108,23 @@ function main() {
 
   // Thiết lập điểm mắt và khối quan sát
   var viewProjMatrix = new Matrix4();
+  ///// start thêm 5 //////////
   var modelMatrix = new Matrix4();  // Model matrix
   var mvpMatrix = new Matrix4();    // Model view projection matrix
   var normalMatrix = new Matrix4(); // Transformation matrix for normals
+  ///// end thêm 5 //////////
 
   viewProjMatrix.setPerspective(30.0, canvas.width / canvas.height, 1.0, 100.0); // Phép chiếu
   viewProjMatrix.lookAt(3.0, 3.0, 7.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Góc nhìn quan sát
 
-/////
-// Set the light color (white)
+  ///// start thêm 6 //////////
+  // Set the light color (white)
   gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0);
   // Set the light direction (in the world coordinate)
   gl.uniform3f(u_LightPosition, 3.0, 3.0, 7.0);
   // Set the ambient light
   gl.uniform3f(u_AmbientLight, 0.2, 0.2, 0.2);
-
-/////
+  
   // Pass the model view projection matrix to the variable u_MvpMatrix
   gl.uniformMatrix4fv(u_MvpMatrix, false, viewProjMatrix.elements);
 
@@ -134,6 +132,7 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);   // Draw the cube
+  ///// end thêm 6 //////////
 
   // Khởi tạo bề mặt
   gl.uniform1i(u_PickedFace, -1);
@@ -144,36 +143,17 @@ function main() {
     var rect = ev.target.getBoundingClientRect();
     if (rect.left <= x && x < rect.right && rect.top <= y && y < rect.bottom) {
       // Nếu vị trí Đã nhấp nằm bên trong <canvas>, hãy cập nhật bề mặt đã chọn
+      ///// start sửa 2 (thêm giá trị truyền vào modelMatrix, u_ModelMatrix, mvpMatrix, u_NormalMatrix, normalMatrix vào hàm) //////////
       updatePickedFace(gl, n, x - rect.left, rect.bottom - y, u_PickedFace, viewProjMatrix, u_MvpMatrix, modelMatrix, u_ModelMatrix, mvpMatrix, u_NormalMatrix, normalMatrix);
+      ///// end sửa 2 //////////
     }
   }
 
   var tick = function() {   // Start drawing
     g_currentAngle = animate(g_currentAngle);
+    ///// start sửa 3 (thêm giá trị truyền vào modelMatrix, u_ModelMatrix, mvpMatrix, u_NormalMatrix, normalMatrix vào hàm) //////////
     draw(gl, n, g_currentAngle, viewProjMatrix, u_MvpMatrix, modelMatrix, u_ModelMatrix, mvpMatrix, u_NormalMatrix, normalMatrix);
-/////
-    // Calculate the model matrix
-    // modelMatrix.setRotate(g_currentAngle, 0.0, 1.0, 0.0); // Rotate around the y-axis
-    // modelMatrix.rotate(g_angleStepUD, 1.0, 0.0, 0.0);
-
-    // // Pass the model matrix to u_ModelMatrix
-    // gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
-
-    // // Pass the model view projection matrix to u_MvpMatrix
-    // mvpMatrix.set(viewProjMatrix).multiply(modelMatrix);
-    // gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
-
-    // // Pass the matrix to transform the normal based on the model matrix to u_NormalMatrix
-    // normalMatrix.setInverseOf(modelMatrix);
-    // normalMatrix.transpose();
-    // gl.uniformMatrix4fv(u_NormalMatrix, false, normalMatrix.elements);
-
-    // // Clear color and depth buffer
-    // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // // Draw the cube
-    // gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
-/////
+    ///// end sửa 3 //////////
     requestAnimationFrame(tick, canvas);
   };
   tick();
@@ -198,6 +178,7 @@ function initVertexBuffers(gl) {
      1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0     // v4-v7-v6-v5 back
   ]);
 
+  ///// start thêm 7 //////////
   // Normal
   var normals = new Float32Array([
     0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
@@ -207,6 +188,7 @@ function initVertexBuffers(gl) {
     0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
     0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
   ]);
+  ///// end thêm 7 //////////
 
   var colors = new Float32Array([   // Màu
      1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,    // v0-v1-v2-v3 front
@@ -247,7 +229,9 @@ function initVertexBuffers(gl) {
   if (!initArrayBuffer(gl, vertices, gl.FLOAT, 3, 'a_Position')) return -1; // Coordinate Information
   if (!initArrayBuffer(gl, colors, gl.FLOAT, 3, 'a_Color')) return -1;      // Color Information
   if (!initArrayBuffer(gl, faces, gl.UNSIGNED_BYTE, 1, 'a_Face')) return -1;// Surface Information
+  ///// start thêm 8 //////////
   if (!initArrayBuffer(gl, normals, gl.FLOAT, 3, 'a_Normal')) return -1;
+  ///// end thêm 8 //////////
 
   // Bỏ liên kết bộ đệm đối tượng 
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
@@ -259,17 +243,18 @@ function initVertexBuffers(gl) {
   return indices.length;
 }
 
-// Phát hiện bề mặt nào được chọn ([Trả lại số bề mặt theo vị trí của điểm])
+///// start sửa 4 (thêm giá trị truyền vào modelMatrix, u_ModelMatrix, mvpMatrix, u_NormalMatrix, normalMatrix vào hàm) //////////
 function updatePickedFace(gl, n, x, y, u_PickedFace, viewProjMatrix, u_MvpMatrix, modelMatrix, u_ModelMatrix, mvpMatrix, u_NormalMatrix, normalMatrix) {
+  ///// end sửa 4 //////////
   var pixels = new Uint8Array(4); // Mảng để lưu trữ giá trị pixel [R,G,B,A]
 
   // Ghi số bề mặt(A) vào thành phần (nếu được chọn) 
   // Sau khi nhấp chuột, biến u_PickedFace sẽ được thay đổi từ -1 thành 0
   gl.uniform1i(u_PickedFace, 0);  // Vẽ bằng cách viết số bề mặt thành giá trị alpha
 
-  // Lúc này, giá trị của mỗi bề mặt phụ thuộc vào số bề mặt 
-  // (bước vẽ này sẽ được thực hiện trong bộ đệm màu và sẽ không hiển thị trên màn hình)
+  ///// start sửa 5 (thêm giá trị truyền vào modelMatrix, u_ModelMatrix, mvpMatrix, u_NormalMatrix, normalMatrix vào hàm) //////////
   draw(gl, n, g_currentAngle, viewProjMatrix, u_MvpMatrix, modelMatrix, u_ModelMatrix, mvpMatrix, u_NormalMatrix, normalMatrix);
+  ///// end sửa 5 //////////
 
   // Đọc giá trị pixel của vị trí được nhấp. pixel [3] là số bề mặt
   gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
@@ -277,18 +262,11 @@ function updatePickedFace(gl, n, x, y, u_PickedFace, viewProjMatrix, u_MvpMatrix
 }
 
 var g_MvpMatrix = new Matrix4(); // Ma trận chiếu chế độ xem mô hình
+  ///// start sửa 6 (thêm giá trị truyền vào modelMatrix, u_ModelMatrix, mvpMatrix, u_NormalMatrix, normalMatrix vào hàm) //////////
 function draw(gl, n, currentAngle, viewProjMatrix, u_MvpMatrix, modelMatrix, u_ModelMatrix, mvpMatrix, u_NormalMatrix, normalMatrix) {
-  // Tính toán Ma trận chiếu ở chế độ chuyển động và truyền nó tới u_MvpMatrix
-  // g_MvpMatrix.set(viewProjMatrix);
+  ///// end sửa 6 //////////
 
-  // g_MvpMatrix.rotate(g_angleStepUD, 0.0, 0.0, 1.0);
-
-  // g_MvpMatrix.rotate(g_currentAngle, 0.0, 1.0, 0.0);
-
-  // gl.uniformMatrix4fv(u_MvpMatrix, false, g_MvpMatrix.elements);
-
-  // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);     // Vùng đệm màu nền và chiều sâu
-  // gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);   // Vẽ hình lập phương
+    ///// start sửa 7 //////////
     modelMatrix.setRotate(g_currentAngle, 0.0, 1.0, 0.0); // Rotate around the y-axis
     modelMatrix.rotate(g_angleStepUD, 1.0, 0.0, 0.0);
 
@@ -303,6 +281,7 @@ function draw(gl, n, currentAngle, viewProjMatrix, u_MvpMatrix, modelMatrix, u_M
     normalMatrix.setInverseOf(modelMatrix);
     normalMatrix.transpose();
     gl.uniformMatrix4fv(u_NormalMatrix, false, normalMatrix.elements);
+    ///// end sửa 7 //////////
 
     // Clear color and depth buffer
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -346,6 +325,7 @@ function initArrayBuffer (gl, data, type, num, attribute) {
   return true;
 }
 
+///// start thêm 9 //////////
 function up() {
   g_angleStepUD -= 15.0;
 }
@@ -365,4 +345,4 @@ function left() {
 function stop() {
   ANGLE_STEP = 0.0;
 }
-
+///// end thêm 9 //////////
